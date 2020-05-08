b9478e3cb5c0da67f257f8267df84c27fd679a66
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2020-05-08 23:52:50 +0300

    adds app dependencies

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const sanitizer = require("express-sanitizer");
+const xss = require("xss-clean");
+const cors = require("cors");
+const request = require("request-promise");
 
 //Import our models
 const {
@@ -11,16 +15,33 @@ const {
 } = require("./models/sequelize.js");
 
 const app = express();
-app.use(bodyParser.json());
 
-// API ENDPOINTS
+// -------------------Enable CORS for all origins-------------------
+app.use(cors());
+
+// -------------------Parsing APP data-------------------
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/json" }));
+
+// -------------------Data sanitization against SQL injection attacks-------------------
+app.use(sanitizer());
+
+// -------------------Data sanitization against xss attacks-------------------
+app.use(xss());
+
+// GLOBAL MIDDLEWARE
+
 app.get("/api", (req, res) => {
-  res.send("Welcome to the Crash detection system");
+  res.json({ message: "Welcome to the Emergency alerts system" });
 });
+
 //admin routes
 app.post("/api/admin", (req, res) => {
   admin.create(req.body).then((admin) => res.json(admin));
 });
+
 // get all admins
 app.get("/api/admin", (req, res) => {
   admin.findAll().then((admins) => res.json(admins));
