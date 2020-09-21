9c25f14978f0da03c9d1e58271952c0a957a85ee
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2020-09-22 02:17:32 +0300

    removes typo

--- auth/userAuthProtector.js
+++ auth/userAuthProtector.js
@@ -1,4 +1,4 @@
-const AppError = require('./../utils/AppError');
+const AppError = require('./../utils/appError');
 const { promisfy } = require('util');
 const jwt = require('jsonwebtoken');
 const catchAsync = require('./../utils/catchAsync');
