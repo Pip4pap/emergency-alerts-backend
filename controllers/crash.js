9978b73e2dfc6568213fb6b25dbd019a8b5296aa
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2020-10-12 18:29:12 +0300

    changes addCrash route to use google maps API

--- controllers/crash.js
+++ controllers/crash.js
@@ -1,7 +1,8 @@
-const { Crash } = require("./../models/sequelize");
-const AppError = require("./../utils/appError");
-const catchAsync = require("./../utils/catchAsync");
-const { Op } = require("sequelize");
+const { Crash } = require('./../models/sequelize');
+const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync');
+const { Op } = require('sequelize');
+const GM = require('./../utils/googlemaps');
 
 module.exports = {
   addCrash: catchAsync(async (req, res, next) => {
@@ -10,7 +11,7 @@ module.exports = {
 
     // crash.addHospital(,{through:{status:"viewing"}})
     res.status(201).json({
-      status: "success",
+      status: 'success',
       data: crash,
     });
   }),
@@ -18,18 +19,17 @@ module.exports = {
     crashes = await Crash.findAll();
 
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: crashes,
     });
   }),
   getCrash: catchAsync(async (req, res, next) => {
     crash = await Crash.findByPk(req.params.id);
 
-    if (!crash)
-      return next(new AppError("No crash exists with such an ID", 404));
+    if (!crash) return next(new AppError('No crash exists with such an ID', 404));
 
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: crash,
     });
   }),
@@ -42,20 +42,27 @@ module.exports = {
       },
     });
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: crashes,
     });
   }),
   addEmergencyCrash: catchAsync(async (req, res, next) => {
-    crash = await Crash.create({
-      riderID: req.params.id,
-      crash_latitude: req.params.latitude,
-      crash_longitude: req.params.longitude,
+    let crashDetails = {
+      RiderID: req.params.id,
+      crashLatitude: req.params.latitude,
+      crashLongitude: req.params.longitude,
       timestamp: req.params.timeStamp,
-    });
+    };
+    crashDetails.crashPlaceID = await GM.getAccidentPlaceId(
+      crashDetails.crashLatitude,
+      crashDetails.crashLongitude
+    );
+    crashDetails.crashPlaceName = await GM.getAccidentPlaceName(crashDetails.crashPlaceID);
+
+    const crash = await Crash.create(crashDetails);
 
     res.status(201).json({
-      status: "success",
+      status: 'success',
       data: crash,
     });
   }),
