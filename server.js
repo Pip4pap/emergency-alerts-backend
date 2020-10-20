b393c7ef1c58a116e7d689b53f0955b2e7c4a064
pip4pap <phillip.musumba.52@gmail.com>
2020-10-20 16:03:30 +0300

    Published super admin authentication endpoints

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const request = require('request-promise');
 const dotenv = require('dotenv');
 
 // Set the env variables
-dotenv.config({ path: '.env' });
+dotenv.config({path: '.env'});
 
 const AppError = require('./utils/appError');
 const hospitalAdminRouter = require('./routes/hospital_admin');
+const admin = require('./routes/admin.js')
 const hospitalRouter = require('./routes/hospital');
 const riderRouter = require('./routes/rider');
 const crashRouter = require('./routes/crash');
@@ -25,9 +26,9 @@ app.use(cors());
 
 // -------------------Parsing APP data-------------------
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.json({type: 'application/json'}));
 
 // -------------------Data sanitization against SQL injection attacks-------------------
 app.use(sanitizer());
@@ -38,61 +39,54 @@ app.use(xss());
 // GLOBAL MIDDLEWARE
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Welcome to the Emergency alerts system' });
+    res.json({message: 'Welcome to the Emergency alerts system'});
 });
 
 app.use('/api/hospitalAdmin', hospitalAdminRouter);
+app.use('/api/admin', admin)
 app.use('/api/hospital', hospitalRouter);
 app.use('/api/rider', riderRouter);
 app.use('/api/crash', crashRouter);
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Cant find ${req.originalUrl} on this server`, 404));
+    next(new AppError(`Cant find ${
+        req.originalUrl
+    } on this server`, 404));
 });
 
 app.use(globalErrorHandler);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`Running on http://localhost:${port}`);
+    console.log(`Running on http://localhost:${port}`);
 });
 
-//Creation of the web socket
-const wsServer = new webSocket({
-  httpServer: server,
-  autoAcceptConnections: true,
-});
+// Creation of the web socket
+const wsServer = new webSocket({httpServer: server, autoAcceptConnections: true});
 
 wsServer.on('connect', (connection) => {
-  connection.on('message', (message) => {
-    console.log('Received Message:', message.utf8Data);
-    connection.sendUTF(message.utf8Data);
-    console.log(
-      'IF MESSAGE IS RECIECVED FROM HOSPITAL TO ACCEPT A CRASH, CHANGE ITS STATUS TO ACCEPTED AND OTHERS AS VIEWED'
-    );
-  });
+    connection.on('message', (message) => {
+        console.log('Received Message:', message.utf8Data);
+        connection.sendUTF(message.utf8Data);
+        console.log('IF MESSAGE IS RECIECVED FROM HOSPITAL TO ACCEPT A CRASH, CHANGE ITS STATUS TO ACCEPTED AND OTHERS AS VIEWED');
+    });
 
-  connection.on('close', function (reasonCode, description) {
-    console.log('Client has disconnected.');
-  });
+    connection.on('close', function (reasonCode, description) {
+        console.log('Client has disconnected.');
+    });
 
-  setInterval(
-    catchAsync(async () => {
-      console.log('GET ALL EMERGENCY CRASHES');
-      console.log('fILTER OUT THOSE THAT ARE CLOSE TO HOSPITAL');
-      console.log('ADD THE FILTERED CRASHES TO HOSPITAL WITH DEFAULT STATUS PENDING');
-      console.log(
-        'RETURN ONLY THE EM CRASHES THAT HAVE BEEN ATTACHED TO A HOSPITAL BACK TO THE CLIENT WITH STATUS PENDING'
-      );
-      console.log(
-        'IF A CRASH IS NOT ACCEPTED INgit  30 MINS ITS STATUS SHOULD CHANGE TO VIEWED IN ALL ATTACHED HOSPITALS'
-      );
-    }),
-    5000
-  );
+    setInterval(catchAsync(async () => {
+        console.log('GET ALL EMERGENCY CRASHES');
+        console.log('fILTER OUT THOSE THAT ARE CLOSE TO HOSPITAL');
+        console.log('ADD THE FILTERED CRASHES TO HOSPITAL WITH DEFAULT STATUS PENDING');
+        console.log('RETURN ONLY THE EM CRASHES THAT HAVE BEEN ATTACHED TO A HOSPITAL BACK TO THE CLIENT WITH STATUS PENDING');
+        console.log('IF A CRASH IS NOT ACCEPTED INgit  30 MINS ITS STATUS SHOULD CHANGE TO VIEWED IN ALL ATTACHED HOSPITALS');
+    }), 5000);
 });
 wsServer.on('close', (connection, closeReason, description) => {
-  console.log(`Peer ${connection.remoteAddress} disconnected.`);
+    console.log(`Peer ${
+        connection.remoteAddress
+    } disconnected.`);
 });
 
 module.exports = app;
--- test/generators.js
+++ test/generators.js
@@ -1,17 +1,19 @@
 exports.newHospital = {
-  ID: '32f03068-770a-45a8-9e16-f59d2c9b9930',
-  hospitalName: 'Mulago Hospital',
-  hospitalLatitude: '0.23435',
-  hospitalLongitude: '35.345235',
+    ID: '32f03068-770a-45a8-9e16-f59d2c9b9930',
+    hospitalName: 'Mulago Hospital',
+    hospitalLatitude: '0.23435',
+    hospitalLongitude: '35.345235'
 };
 
+exports.newAdmin = {}
+
 exports.newHospitalAdmin = {
-  ID: '1321494d-e8c5-44b8-bb71-057ae9be19e0',
-  firstname: 'Calvin',
-  lastname: 'Karegyeya',
-  email: 'calvinkaregyeya@gmail.com',
-  password: '12345678',
-  passwordConfirm: '12345678',
+    ID: '1321494d-e8c5-44b8-bb71-057ae9be19e0',
+    firstname: 'Calvin',
+    lastname: 'Karegyeya',
+    email: 'calvinkaregyeya@gmail.com',
+    password: '12345678',
+    passwordConfirm: '12345678'
 };
 
 exports.newPolice = [];
@@ -19,21 +21,21 @@ exports.newPolice = [];
 exports.newPoliceAdmin = [{}];
 
 exports.newCrash = {
-  ID: '197541dc-8d18-4f12-b189-621a11a958f8',
-  crash_latitude: '0.4253',
-  crash_longitude: '32.45454',
-  timestamp: '2021-06-07T15:21:28.000Z',
+    ID: '197541dc-8d18-4f12-b189-621a11a958f8',
+    crash_latitude: '0.4253',
+    crash_longitude: '32.45454',
+    timestamp: '2021-06-07T15:21:28.000Z'
 };
 
 exports.newRider = {
-  ID: 'ad0ae81b-51ac-4241-9ec7-49f94280f217',
-  firstname: 'David',
-  lastname: 'Masiga',
-  email: 'davidmasiga@gmail.com',
-  password: '12345678',
-  passwordConfirm: '12345678',
-  next_Of_kin_Name: 'Calvin',
-  next_Of_kin_Contact: '0753500046',
+    ID: 'ad0ae81b-51ac-4241-9ec7-49f94280f217',
+    firstname: 'David',
+    lastname: 'Masiga',
+    email: 'davidmasiga@gmail.com',
+    password: '12345678',
+    passwordConfirm: '12345678',
+    next_Of_kin_Name: 'Calvin',
+    next_Of_kin_Contact: '0753500046'
 };
 
 exports.newHospitalCrashes = [{}];
