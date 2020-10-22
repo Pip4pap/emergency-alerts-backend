ce74f8fc418f0b5df0671aed986cda1972b8835f
pip4pap <phillip.musumba.52@gmail.com>
2020-10-22 15:41:36 +0300

    Fixes bug that prevents hospital admin from requesting for hospital attachment

--- models/hospital_admin.js
+++ models/hospital_admin.js
@@ -4,118 +4,109 @@ const crypto = require('crypto');
 const AppError = require('./../utils/appError.js');
 
 module.exports = function (sequelize, DataTypes) {
-  let HospitalAdmin = sequelize.define(
-    'HospitalAdmin',
-    {
-      ID: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
-      },
-      firstname: DataTypes.STRING(40),
-      lastname: DataTypes.STRING(40),
-      email: {
-        type: DataTypes.STRING(45),
-        allowNull: false,
-        unique: true,
-        validate: { isEmail: { msg: 'Please provide a valid email' } },
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      passwordConfirm: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          passwordsMatch: function (passConfirm) {
-            if (passConfirm !== this.password) throw new Error('Passwords do not match!');
-          },
+    let HospitalAdmin = sequelize.define('HospitalAdmin', {
+        ID: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
         },
-      },
-      role: {
-        type: DataTypes.ENUM('HospitalAdmin'),
-        defaultValue: 'HospitalAdmin',
-      },
-      verified: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-      },
-      verificationStatus: {
-        type: DataTypes.ENUM('Approved', 'Pending', 'Denied'),
-        defaultValue: 'Pending',
-      },
-      passwordChangedAt: DataTypes.DATE,
-      passwordResetToken: DataTypes.STRING,
-      passwordResetExpires: DataTypes.DATE,
-    },
-    {
-      tableName: 'HospitalAdmin',
-      hooks: {
-        beforeBulkCreate: async function (users, options) {
-          for (user of users) {
-            user.password = await bcrypt.hash(user.password, 12);
-            // Delete passwordConfirm field and do not save it to DB
-            user.passwordConfirm = '';
-          }
+        firstname: DataTypes.STRING(40),
+        lastname: DataTypes.STRING(40),
+        email: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: 'Please provide a valid email'
+                }
+            }
         },
-        afterValidate: async function (user, options) {
-          //Run this only if password has changed
-          console.log('After Validate');
-          console.log(user.changed('password'));
-          if (user.changed('password') || user.isNewRecord || !user.changed('password')) {
-            user.password = await bcrypt.hash(user.password, 12);
-            // Delete passwordConfirm field and do not save it to DB
-            user.passwordConfirm = '';
-            user.passwordChangedAt = Date.now();
-          }
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
         },
-        afterSave: async function (user, options) {
-          //Run this only if password has changed
-          console.log('Before Save');
-          console.log(user.changed('password'));
-          if (user.changed('password') || user.isNewRecord) {
-            user.password = await bcrypt.hash(user.password, 12);
-            // Delete passwordConfirm field and do not save it to DB
-            user.passwordConfirm = '';
-            user.passwordChangedAt = Date.now();
-          }
-        },
-      },
-    }
-  );
+        passwordConfirm: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                passwordsMatch: function (passConfirm) {
+                    if (passConfirm !== this.password) 
+                        throw new Error('Passwords do not match!');
+                    
 
-  // class methods
-  HospitalAdmin.associate = function (models) {
-    HospitalAdmin.belongsTo(models.Hospital, {
-      foreignKey: 'HospitalID',
+
+                }
+            }
+        },
+        role: {
+            type: DataTypes.ENUM('HospitalAdmin'),
+            defaultValue: 'HospitalAdmin'
+        },
+        verified: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        verificationStatus: {
+            type: DataTypes.ENUM('Approved', 'Pending', 'Denied'),
+            defaultValue: 'Pending'
+        },
+        passwordChangedAt: DataTypes.DATE,
+        passwordResetToken: DataTypes.STRING,
+        passwordResetExpires: DataTypes.DATE
+    }, {
+        tableName: 'HospitalAdmin',
+        hooks: {
+            beforeBulkCreate: async function (users, options) {
+                for (user of users) {
+                    user.password = await bcrypt.hash(user.password, 12);
+                    // Delete passwordConfirm field and do not save it to DB
+                    user.passwordConfirm = '';
+                }
+            },
+            afterSave: async function (user, options) { // Run this only if password has changed
+                console.log('After Validate');
+                console.log(user.changed('password'));
+                console.log(user.isNewRecord)
+                if (user.changed('password') || user.isNewRecord) {
+                    user.password = await bcrypt.hash(user.password, 12);
+                    // Delete passwordConfirm field and do not save it to DB
+                    user.passwordConfirm = '';
+                    user.passwordChangedAt = Date.now();
+                }
+            }
+        }
     });
-  };
 
-  HospitalAdmin.prototype.isPasswordCorrect = async function (passwordToCheck) {
-    return await bcrypt.compare(passwordToCheck, this.password);
-  };
+    // class methods
+    HospitalAdmin.associate = function (models) {
+        HospitalAdmin.belongsTo(models.Hospital, {foreignKey: 'HospitalID'});
+    };
 
-  HospitalAdmin.prototype.createPasswordResetToken = function () {
-    const resetToken = crypto.randomBytes(32).toString('hex');
+    HospitalAdmin.prototype.isPasswordCorrect = async function (passwordToCheck) {
+        return await bcrypt.compare(passwordToCheck, this.password);
+    };
 
-    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    HospitalAdmin.prototype.createPasswordResetToken = function () {
+        const resetToken = crypto.randomBytes(32).toString('hex');
 
-    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+        this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
 
-    return resetToken;
-  };
+        this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
-  HospitalAdmin.prototype.isPasswordChangedAfterTokenIssued = function (jwtTimeStamp) {
-    if (this.passwordChangedAt) {
-      const changedTimeStamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
-      return jwtTimeStamp < changedTimeStamp;
-    }
+        return resetToken;
+    };
 
-    //False if password was never changed
-    return false;
-  };
+    HospitalAdmin.prototype.isPasswordChangedAfterTokenIssued = function (jwtTimeStamp) {
+        if (this.passwordChangedAt) {
+            const changedTimeStamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+            return jwtTimeStamp < changedTimeStamp;
+        }
 
-  return HospitalAdmin;
+        // False if password was never changed
+        return false;
+    };
+
+    return HospitalAdmin;
 };
