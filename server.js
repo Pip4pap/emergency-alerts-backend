6ced5b139f5df3ff8cea320b9f4d0bd968b89fc8
Calvin Karegyeya <calvinkaregyeya@gmail.com>
2020-10-22 15:54:19 +0300

    adds websocket to app

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const webSocket = require('websocket').server;
-const catchAsync = require('./utils/wsCatchAsync.js');
 const bodyParser = require('body-parser');
 const sanitizer = require('express-sanitizer');
 const xss = require('xss-clean');
@@ -17,6 +16,8 @@ const hospitalRouter = require('./routes/hospital');
 const riderRouter = require('./routes/rider');
 const crashRouter = require('./routes/crash');
 const globalErrorHandler = require('./controllers/errorController');
+
+const webSocketServer = require('./webSocket.js');
 
 const app = express();
 
@@ -58,41 +59,6 @@ const server = app.listen(port, () => {
 });
 
 //Creation of the web socket
-const wsServer = new webSocket({
-  httpServer: server,
-  autoAcceptConnections: true,
-});
-
-wsServer.on('connect', (connection) => {
-  connection.on('message', (message) => {
-    console.log('Received Message:', message.utf8Data);
-    connection.sendUTF(message.utf8Data);
-    console.log(
-      'IF MESSAGE IS RECIECVED FROM HOSPITAL TO ACCEPT A CRASH, CHANGE ITS STATUS TO ACCEPTED AND OTHERS AS VIEWED'
-    );
-  });
-
-  connection.on('close', function (reasonCode, description) {
-    console.log('Client has disconnected.');
-  });
-
-  setInterval(
-    catchAsync(async () => {
-      console.log('GET ALL EMERGENCY CRASHES');
-      console.log('fILTER OUT THOSE THAT ARE CLOSE TO HOSPITAL');
-      console.log('ADD THE FILTERED CRASHES TO HOSPITAL WITH DEFAULT STATUS PENDING');
-      console.log(
-        'RETURN ONLY THE EM CRASHES THAT HAVE BEEN ATTACHED TO A HOSPITAL BACK TO THE CLIENT WITH STATUS PENDING'
-      );
-      console.log(
-        'IF A CRASH IS NOT ACCEPTED INgit  30 MINS ITS STATUS SHOULD CHANGE TO VIEWED IN ALL ATTACHED HOSPITALS'
-      );
-    }),
-    5000
-  );
-});
-wsServer.on('close', (connection, closeReason, description) => {
-  console.log(`Peer ${connection.remoteAddress} disconnected.`);
-});
+webSocketServer(server);
 
 module.exports = app;
