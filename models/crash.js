d5b8a65397fe2c2f8a80f6a01918d6bd801cf793
pip4pap <phillip.musumba.52@gmail.com>
2020-11-12 18:51:46 +0300

    Creates relationship between Police and Crash models through PoliceCrash model

--- models/crash.js
+++ models/crash.js
@@ -1,50 +1,43 @@
 /* jshint indent: 1 */
 
 module.exports = function (sequelize, DataTypes) {
-  let Crash = sequelize.define(
-    'Crash',
-    {
-      ID: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
-      },
-      crashLatitude: {
-        type: DataTypes.STRING(45),
-        allowNull: false,
-      },
-      crashLongitude: {
-        type: DataTypes.STRING(45),
-        allowNull: false,
-      },
-      crashPlaceName: {
-        type: DataTypes.STRING(45),
-        allowNull: true,
-      },
-      crashPlaceID: {
-        type: DataTypes.STRING(45),
-        allowNull: true,
-      },
-      // TODO: Change this date
-      timestamp: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Date.now,
-      },
-    },
-    {
-      tableName: 'Crash',
-    }
-  );
+    let Crash = sequelize.define('Crash', {
+        ID: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        crashLatitude: {
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        crashLongitude: {
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        crashPlaceName: {
+            type: DataTypes.STRING(45),
+            allowNull: true
+        },
+        crashPlaceID: {
+            type: DataTypes.STRING(45),
+            allowNull: true
+        },
+        // TODO: Change this date
+        timestamp: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: Date.now
+        }
+    }, {tableName: 'Crash'});
 
-  // Class methods
-  Crash.associate = function (models) {
-    Crash.belongsTo(models.Rider, {
-      foreignKey: 'RiderID',
-    });
-    Crash.belongsToMany(models.Hospital, { through: models.HospitalCrash });
-  };
+    // Class methods
+    Crash.associate = function (models) {
+        Crash.belongsTo(models.Rider, {foreignKey: 'RiderID'});
+        Crash.belongsToMany(models.Hospital, {through: models.HospitalCrash});
+        Crash.belongsToMany(models.Police, {through: models.PoliceCrash});
+    };
 
-  return Crash;
+    return Crash;
 };
