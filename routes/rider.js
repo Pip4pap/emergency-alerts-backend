4790b437031232c1c71e9fdaebc15ae2f2a696a8
pip4pap <phillip.musumba.52@gmail.com>
2020-11-26 10:44:43 +0300

    Creates a log in route for riders

--- routes/rider.js
+++ routes/rider.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const controller = require("./../controllers/rider");
+const appAuthController = require('./../auth/userAuthController');
 const appAuthProtector = require('./../auth/userAuthProtector');
+const {Rider} = require('./../models/sequelize');
+
+const RiderAuth = new appAuthController(Rider);
 
 const router = express.Router({mergeParams: true});
+
+router.post('/login', RiderAuth.login());
+router.get('/logout', RiderAuth.logout());
+
 router.use(appAuthProtector());
+
 router.route("/").get(controller.getAllRiders).post(controller.addRider);
 router.get("/:id", controller.getRider);
 module.exports = router;
