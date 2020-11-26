9e3e7a8eb49c014f630f5065609128e8073eb580
pip4pap <phillip.musumba.52@gmail.com>
2020-11-26 10:29:14 +0300

    Protects the rider signup route so that only a logged in admin can register riders

--- routes/rider.js
+++ routes/rider.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const controller = require("./../controllers/rider");
+const appAuthProtector = require('./../auth/userAuthProtector');
 
-const router = express.Router({ mergeParams: true });
+const router = express.Router({mergeParams: true});
+router.use(appAuthProtector());
 router.route("/").get(controller.getAllRiders).post(controller.addRider);
 router.get("/:id", controller.getRider);
 module.exports = router;
